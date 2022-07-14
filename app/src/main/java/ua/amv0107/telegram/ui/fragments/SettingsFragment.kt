73897406628f34701b72accb07d4b89bd43223e6
amv0107 <amv0107@gmail.com>
2022-07-14 09:47:31 +0300

    7. Добавляем Options menu с иконками.

--- app/src/main/java/ua/amv0107/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/ua/amv0107/telegram/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,17 @@
 package ua.amv0107.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import ua.amv0107.telegram.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }
